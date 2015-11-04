eb9e8d7de598be490a5eb85f15679ebcb1373613
Sergey <kuchinsergey5002@gmail.com>
2015-11-05 00:09:53 +0500

    попытка записать в эксель

--- src/ru/sergey/excel/SendDataToExcel.java
+++ src/ru/sergey/excel/SendDataToExcel.java
@@ -1,11 +1,6 @@
 package ru.sergey.excel;
 
 
-import org.apache.poi.ddf.EscherColorRef;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.util.CellReference;
-import org.apache.poi.xssf.usermodel.XSSFShape;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import ru.sergey.data.DefaultData;
@@ -13,27 +8,21 @@ import ru.sergey.data.DefaultData;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Iterator;
 
 public class SendDataToExcel {
     public void sendData() {
         FileInputStream file = null;
         double[][] array = DefaultData.getDefaultArray();
         try {
-            file = new FileInputStream(new File("Книга1.xlsx"));
+            file = new FileInputStream(new File("optimize.xlsm"));
             XSSFWorkbook workbook = new XSSFWorkbook(file);
             XSSFSheet sheet = workbook.getSheetAt(0);
-            int i = 0;
-            for (Row row : sheet) {
-                int j = 0;
+            int rowNum = 3;
+            int maxRowNum = 12;
+            for(int i = rowNum; i <= maxRowNum; i++) {
+                for (int j = 1; j <= 8; j++) {
 
-                for (Cell cell : row) {
-                    if(cell.getNumericCellValue() != 0.0) {
-
-                        j++;
-                    }
                 }
-                i++;
             }
         } catch (IOException e) {
             e.printStackTrace();
